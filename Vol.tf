8f65ef6308535b745675603019e5dd1bf56cfdc1
Sheshi Vardhan <sheshi@tcs.com>
2026-05-07 07:45:42 +0530

    Added new Resource

--- Vol.tf
+++ Vol.tf
@@ -4,6 +4,16 @@ resource "aws_ebs_volume" "v1"{
 	size=2
 	availability_zone="ap-south-1a"
 	tags={
-		Name="TFVolume"
+		Name="TFVolume-0"
+		owner="Sheshivardhan"
+	}
+}
+
+resource "aws_ebs_volume" "v2"{
+	size=1
+	availability_zone="ap-south-1b"
+	tags={
+		Name="TFVolume-1"
+		owner="Sheshivardhan"
 	}
 }
